deb68bbd33415e83417db8b192154ce54dced6b9
Ferreira <107485754+Ferreiraah@users.noreply.github.com>
2023-07-12 01:53:15 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,46 @@
-const personagens = document.querySelectorAll(".personagem");
+
+
+const personagens = document.querySelectorAll('.personagem');
+
+
 
 personagens.forEach((personagem) => {
-  personagem.addEventListener("mouseenter", () => {
-    if (window.innerWidth < 450) {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    }
+    personagem.addEventListener('mouseenter', () => {
 
-    removerSelecaoDoPersonagem(personagem);
+        if(window.innerWidth < 450) {
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }
+        
+        removerSelecaoDoPersonagem();
 
-    personagem.classList.add("selecionado");
+        personagem.classList.add('selecionado');
+      
+        alterarImagemPersonagemSelecionado(personagem);
+        
+        alterarNomePersonagemSelecionado(personagem);
+      
+        alterarDescricaoPersonagem(personagem);
+    })
+})
 
-    alterarImagemPersonagemSelecionado(personagem);
-
-    alterarNomePersonaogem(personagem);
-
-    alterarDescricaoPersonagem(personagem);
-  });
-});
 function alterarDescricaoPersonagem(personagem) {
-    const descricaoPersonagem = document.getElementById(
-        "descricao__personagem"
-    );
-    descricaoPersonagem.innerText = personagem.getAttribute("data-description");
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
 }
 
-function alterarNomePersonaogem(personagem) {
-    const nomePersonagem = document.getElementById("nome__personagem");
-    nomePersonagem.innerText = personagem.getAttribute("data-name");
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
 }
 
 function alterarImagemPersonagemSelecionado(personagem) {
-    const imagemPersonagemGrande = document.querySelector(
-        ".personagem__grande"
-    );
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    // passo 2 - alterar a imagem do personagem grande
     const idPersonagem = personagem.attributes.id.value;
-    imagemPersonagemGrande.src = `/src/imagens/card-${idPersonagem}.png`;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
 }
 
 function removerSelecaoDoPersonagem() {
-    const personagemSelecionado = document.querySelector(".selecionado");
-    personagemSelecionado.classList.remove("selecionado");
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
 }
-
